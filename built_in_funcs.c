8d5bf10637869608a78304f732af8a1812c9a543
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-07-27 22:22:07 -0700

    debug

--- built_in_funcs.c
+++ built_in_funcs.c
@@ -1,25 +1,64 @@
 #include "shell.h"
 
 /**
+ * is_valid_exit_status - checks if exit status is valid
+ * @str: string to check
+ * Return: 1 if it is valid otherwise 0
+ */
+int is_valid_exit_status(char *str)
+{
+	int i, status;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
+
+	status = _atoi(str);
+	if (status >= 0 && status <= 255)
+		return (1);
+
+	return (0);
+}
+/**
  * hsh_exit - exits the shell
  * @args: the arguments
  *
  * Return: nothing
  */
+
 int hsh_exit(char **args)
 {
-	int i, stat = 0;
+	int i,  stat;
+	const char *error_message = "./hsh: 1: exit: Illegal number: ";
 
 	if (args[1] != NULL)
 	{
-		stat = _atoi(args[1]);
+		if (is_valid_exit_status(args[1]))
+		{
+			stat = atoi(args[1]);
+			for (i = 0; args[i] != NULL; i++)
+				free(args[i]);
+			free(args);
+			exit(stat);
+		}
+		else
+		{
+			write(STDERR_FILENO, error_message,
+			      strlen(error_message));
+			write(STDERR_FILENO, args[1], strlen(args[1]));
+			write(STDERR_FILENO, "\n", 1);
+			for (i = 0; args[i] != NULL; i++)
+				free(args[i]);
+			free(args);
+			exit(2);
+		}
 	}
 	for (i = 0; args[i] != NULL; i++)
-	{
 		free(args[i]);
-	}
 	free(args);
-	exit(stat);
+	exit(0);
 }
 /**
  * hsh_print_env -  prints the current environment
